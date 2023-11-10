d3a2d672f56bebc0de0437d057f94ed73e7a2937
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2023-11-10 01:18:09 -0600

    mistakes done

--- routes/organizations.js
+++ routes/organizations.js
@@ -4,12 +4,7 @@ const organization_controller= require('../controllers/organizations');
 
 const router = express.Router();
 
-<<<<<<< HEAD
-=======
 
-  
-
->>>>>>> 6cf7f5d9b7a410c7ce213e0fe0328b81fd1e8267
  router.get('/', organization_controller.organizations_view_all_Page);
 
 
