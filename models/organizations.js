16bee06fd7e1597fc1161bdbc94d7c5850cec08b
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2023-11-24 18:28:03 -0600

    added part 5

--- models/organizations.js
+++ models/organizations.js
@@ -1,10 +1,39 @@
+const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const organizationSchema = mongoose.Schema({
-    organizationName: String,
-    location: String,
-    industry: String,
-    numberOfEmployees: Number,
-    revenue: Number,
-    website: String
-})
+    // organizationName: String,
+    // location: String,
+    // industry: String,
+    // numberOfEmployees: Number,
+    // revenue: Number,
+    // website: String
+
+
+    organizationName: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    industry: {
+        type: String,
+        required: true
+    },
+    numberOfEmployees: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100000
+    },
+    revenue: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    website: {
+        type: String
+    }
+});
 module.exports = mongoose.model("organizations", organizationSchema)
--- routes/organizations.js
+++ routes/organizations.js
@@ -4,14 +4,23 @@ const organization_controller= require('../controllers/organizations');
 
 const router = express.Router();
 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 router.get('/', organization_controller.organizations_view_all_Page );
 router.get('/organizations/:id', organization_controller.organization_detail);
-router.get('/detail', organization_controller.organizations_view_one_Page);
 
-router.get('/create', organization_controller.organization_create_page)
+router.get('/detail',secured, organization_controller.organizations_view_one_Page);
 
-router.get('/update', organization_controller.organization_update_page);
+router.get('/create',secured, organization_controller.organization_create_page)
 
-router.get('/delete', organization_controller.organization_delete_Page);
+router.get('/update',secured, organization_controller.organization_update_page);
+
+router.get('/delete',secured, organization_controller.organization_delete_Page);
 
 module.exports = router;
